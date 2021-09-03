34dbc6d81d4147f284f977e94fbbcc4cecef3b89
Kibakuss <duxabank@gmail.com>
2021-09-03 09:37:39 +0500

    Not ended

---
+++ android/app/src/main/kotlin/com/example/flappy_bird_hour/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flappy_bird_hour
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/bird.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyBird extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 35,
+      width: 35,
+      child: Image.asset("images/icons/a.png"),
+    );
+  }
+}
